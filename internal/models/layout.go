e8960d8967e8732f5d465c5eb6a9ddc8ece1d18a
bharath <bharath@mercari.com>
2019-10-22 15:54:56 +0900

    add(internal): models pkg for storing internal data structs

---
+++ internal/models/layout.go
@@ -0,0 +1,14 @@
+package models
+
+// HomeLayout return information regarding the general display order of different components in view
+type HomeLayout struct {
+	Alignment string  `json:"alignment"`
+	Order OrderMap  `json:"order_map"`
+	ComponentIDs  []ComponentType `json:"components_ids"`
+}
+
+// OrderMap holds information regarding the component size and number of components
+// TODO elaborate and change later
+type OrderMap struct {
+	Blocks  []int `json:"blocks"`
+}
