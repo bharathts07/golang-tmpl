5ab79f226b4025f00bae7728afa956d0e13ebf59
bharath <bharath@mercari.com>
2019-10-22 17:13:55 +0900

    add(database): template interface declaration

---
+++ database/interface.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"context"
+
+	"github.com/bharathts07/pokke/internal/models"
+)
+
+// Client specifies the required methods to be implemented
+type Client interface {
+	GetLayout(ctx context.Context, view string) (models.Layout,error)
+	GetComponents(ctx context.Context, view string) ([]models.Component,error)
+}
