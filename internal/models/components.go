ccc949d775825d76684ad51ea70c1741a8d11629
bharath <bharath@mercari.com>
2019-10-23 09:42:41 +0900

    update(internal): Component fields data type signature

--- internal/models/components.go
+++ internal/models/components.go
@@ -6,13 +6,14 @@ type ComponentType int
 const (
 	Banner    ComponentType = 1
 	MenuBar   ComponentType = 2
-	TestBlock ComponentType = 3
+	Body      ComponentType = 3
+	Footer    ComponentType = 4
 )
 
 // Component holds complete information of a single displayable component
 type Component struct {
 	Type ComponentType `json:"type"`
-	Data *interface{}  `json:"data"`
+	Data interface{}  `json:"data"`
 }
 
 // Header defines information to be displayed at the top of any view
