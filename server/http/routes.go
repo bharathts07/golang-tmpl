2218bc78cece921859088e2886f1edae36974087
bharath <bharath@mercari.com>
2019-10-23 09:46:29 +0900

    update(server): db from middleware in route handlers

--- server/http/routes.go
+++ server/http/routes.go
@@ -1,14 +1,15 @@
 package http
 
 import (
-	"github.com/bharathts07/pokke/server/handlers"
 	"github.com/gin-gonic/gin"
+
+	"github.com/bharathts07/pokke/server/handlers"
 )
 
 // setupRoutes defines all the rest API endpoints served by this server
 func setupRoutes(router *gin.Engine) {
 
-	router.GET("/", handlers.GetWelcome)
+	router.GET("/", handlers.GetWelcomeComponents)
 	//v1 := router.Group("/v1",)
 	// Group all the resources for displaying home page
 	//home := v1.Group("/home")
