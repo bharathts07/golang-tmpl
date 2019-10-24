c46809f9b06baab5bb4f53bbf093e28429feaadd
bharath <bharathts07@gmail.com>
2019-10-24 17:54:30 +0900

    update(pokke): read server port from env

--- server/http/server.go
+++ server/http/server.go
@@ -1,9 +1,14 @@
 package http
 
-import "github.com/bharathts07/pokke/database"
+import (
+	"github.com/bharathts07/pokke/config"
+	"github.com/bharathts07/pokke/database"
+	"strconv"
+)
 
 // StartServer starts given server, supporting graceful shutdown of the server
-func StartServer(db database.Client) {
+func StartServer(db database.Client, env *config.Env) {
 	router := CreateRouter(db)
-	_ = router.Run(":8080")
+	serverPort := "0.0.0.0:"+strconv.Itoa(env.GRPCPort)
+	_ = router.Run(serverPort)
 }
