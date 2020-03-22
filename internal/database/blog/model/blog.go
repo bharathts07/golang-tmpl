214a089ecab91da2c6d08d3aa0d7636225d29332
bharath <bharathts07@gmail.com>
2020-03-22 19:56:41 +0900

    update(database): interface implmentation for blogdb
    Firestore implementations

--- internal/database/blog/model/blog.go
+++ internal/database/blog/model/blog.go
@@ -4,24 +4,24 @@ import "time"
 
 // BlogPreview holds enough information for a short preview for display in main page
 type BlogPreview struct {
-	ID      int
-	Title   string
-	Author  string
-	Summary string
-	Tags    []string
-	Photo   string
-	Created time.Time
-	Updated time.Time
+	ID      string    `firestore:"id" json:"id"`
+	Title   string    `firestore:"title" json:"title"`
+	Author  string    `firestore:"author" json:"author"`
+	Summary string    `firestore:"summary" json:"summary"`
+	Tags    []string  `firestore:"tags" json:"tags"`
+	Photo   string    `firestore:"photo" json:"photo"`
+	Created time.Time `firestore:"created" json:"created"`
+	Updated time.Time `firestore:"updated" json:"updated"`
 }
 
 // BlogItem holds complete information for display in the entire content
 type BlogItem struct {
-	ID      int
-	Title   string
-	Author  string
-	Body    string
-	Tags    []string
-	Photo   []string
-	Created time.Time
-	Updated time.Time
+	ID      string    `firestore:"id" json:"id" `
+	Title   string    `firestore:"title" json:"title"`
+	Author  string    `firestore:"author" json:"author"`
+	Body    string    `firestore:"body" json:"body"`
+	Tags    []string  `firestore:"tags" json:"tags"`
+	Photo   []string  `firestore:"photo" json:"photo"`
+	Created time.Time `firestore:"created" json:"created"`
+	Updated time.Time `firestore:"updated" json:"updated"`
 }
