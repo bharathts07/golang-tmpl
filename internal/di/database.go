b60a008e2a5061cb1676eb499ddda5257bebd9e1
bharath <bharathts07@gmail.com>
2020-03-22 19:58:19 +0900

    update(pokke): firestore dependency usage for DI

--- internal/di/database.go
+++ internal/di/database.go
@@ -1,12 +1,15 @@
 package di
 
 import (
+	"cloud.google.com/go/firestore"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 
 	"github.com/bharathts07/pokke/internal/database/blog"
+	"github.com/bharathts07/pokke/internal/database/blog/fire"
 	"github.com/bharathts07/pokke/internal/database/blog/mongodb"
 	"github.com/bharathts07/pokke/models/database"
+	firepkg "github.com/bharathts07/pokke/pkg/firestore"
 	mongopkg "github.com/bharathts07/pokke/pkg/mongodb"
 )
 
@@ -20,18 +23,51 @@ func (c *Container) GetDatabase() database.Client {
 	return c.cache.database
 }
 
+// GetFireStoreClient connects to the firestore database and returns the client
+func (c *Container) GetFireStoreClient() *firestore.Client {
+	if c.cache.fireClient != nil {
+		return c.cache.fireClient
+	}
+
+	client, err := firepkg.NewClient(c.Ctx, c.Env.GCP.ProjectID, c.Env.GCP.CredJSONPath)
+	if err != nil {
+		c.GetLogger().Error("connect to firestore",
+			zap.Error(err),
+		)
+		panic(err)
+	}
+
+	c.GetLogger().Info("connected to firestore")
+	c.cache.fireClient = client
+
+	return c.cache.fireClient
+}
+
 // GetBlogDB calls GetMongoConnection() and returns the business database implementation blog.DB
 func (c *Container) GetBlogDB() blog.DB {
 	if c.cache.blogDB != nil {
 		return c.cache.blogDB
 	}
+	var db blog.DB
+	var err error
 
-	db, err := mongodb.New(c.Ctx, c.GetMongoConnection())
-	if err != nil {
-		c.GetLogger().Error("di.GetBlogDB() :",
-			zap.Error(err),
-		)
-		panic(err)
+	switch c.Env.BlogDBType {
+	case "firestore":
+		db, err = fire.New(c.Ctx, c.GetFireStoreClient())
+		if err != nil {
+			c.GetLogger().Error("di.GetBlogDB() :",
+				zap.Error(err),
+			)
+			panic(err)
+		}
+	case "mongodb":
+		db, err = mongodb.New(c.Ctx, c.GetMongoConnection())
+		if err != nil {
+			c.GetLogger().Error("di.GetBlogDB() :",
+				zap.Error(err),
+			)
+			panic(err)
+		}
 	}
 
 	c.cache.blogDB = db
---
+++ pkg/firestore/firestore.go
@@ -0,0 +1,21 @@
+package firestore
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/option"
+)
+
+func NewClient(ctx context.Context, projectID string, cred string) (*firestore.Client, error) {
+	client, err := firestore.NewClient(
+		ctx,
+		projectID,
+		option.WithCredentialsFile(cred),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
