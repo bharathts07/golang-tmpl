fb6c7ddc10272830df4579a97be78cbd5756a297
bharath <bharathts07@gmail.com>
2022-02-04 11:36:14 +0900

    add(mongodb): createBlog implementation

--- internal/database/blog/mongodb/blogdb_impl.go
+++ internal/database/blog/mongodb/blogdb_impl.go
@@ -2,22 +2,48 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
+
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/bharathts07/pokke/internal/database/blog/model"
 )
 
 func (d db) GetBlogSummary(ctx context.Context, limit int) ([]*model.BlogPreview, error) {
-	panic("implement me")
+	_ = d.client.Database("blog").Collection("blog")
+	return nil, nil
 }
 
 func (d db) GetBlog(ctx context.Context, title string) (*model.BlogItem, error) {
-	panic("implement me")
+	collection := d.client.Database("blog").Collection("blog")
+	var result primitive.M //  an unordered representation of a BSON document which is a Map
+	err := collection.FindOne(ctx, bson.D{{"title", title}}).Decode(&result)
+	if err != nil {
+
+		fmt.Println(err)
+
+	}
+
+	return nil, nil
 }
 
 func (d db) PostBlog(ctx context.Context, item *model.BlogItem) error {
-	panic("implement me")
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	collection := d.client.Database("blog").Collection("blog")
+	res, err := collection.InsertOne(ctx, bson.D{{item.Title, item}})
+	if err != nil {
+		return err
+	}
+	fmt.Printf(fmt.Sprint(res.InsertedID))
+
+	return nil
 }
 
 func (d db) UpdateBlog(ctx context.Context, title string, item *model.BlogItem) error {
-	panic("implement me")
+	_ = d.client.Database("blog").Collection("blog")
+	return nil
 }
